9b380b7ed4d061a7fce274638b0bb9d4fa0e2f57
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-06 20:14:28 +0545

    new

---
+++ Youtube Clone/data/video.js
@@ -0,0 +1,57 @@
+export const video = [
+  {
+    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    thumnail: 'Thumbnail/thumb1.webp',
+    icon: 'icon/icon1.jpg',
+    title: 'Introduction to OOPs in Python | Python Tutorial - Day #56',
+    author: 'CodeWithHarry',
+    veiw: '65K views &#183 7 months ago',
+    time: '2:57'
+  },
+  {
+    id: '15b6fc6f-327a-4ec4-896f-486349e85a3dkp',
+    thumnail: 'Thumbnail/thumb2.webp',
+    icon: 'icon/icon2.jpg',
+    title: 'Sia - Snowman (Lyrics)',
+    author: 'LyricsWaves',
+    veiw: '16K views &#183 7 days ago',
+    time: '2:57'
+  },
+  {
+    id: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    thumnail: 'Thumbnail/thumb3.webp',
+    icon: 'icon/icon3.jpg',
+    title: "my tea's gone cold I'm wondering why",
+    author: '𝙪𝙣𝙝𝙖𝙥𝙥𝙞𝙚𝙧',
+    veiw: '1.8M views &#183 1 year ago',
+    time: '2:57'
+  },
+  {
+    id: '54e0eccd-8f36-462b-b68a-8182611d9add',
+    thumnail: 'Thumbnail/thumb4.webp',
+    icon: 'icon/icon4.jpg',
+    title:
+      ' Gotye - Somebody That I Used To Know (feat. Kimbra) [Official Music Video]',
+    author: 'gotyemusic',
+    veiw: '2.1B views &#183 12 years ago',
+    time: '2:57'
+  },
+  {
+    id: '3ebe75dc-64d2-4137-8860-1f5a963e534b',
+    thumnail: 'Thumbnail/thumb5.webp',
+    icon: 'icon/icon5.jpg',
+    title: 'Lukas Graham - 7 Years [Official Music Video]',
+    author: 'Lukas Graham',
+    veiw: ' 1.4B views &#183 7 years ago',
+    time: '2:57'
+  },
+  {
+    id: '8c9c52b5-5a19-4bcb-a5d1-158a74287c53',
+    thumnail: 'Thumbnail/thumb6.webp',
+    icon: 'icon/icon6.jpg',
+    title: 'Eminem, "Superman" (video lyric)',
+    author: ' Old School HipHop',
+    veiw: ' 170K views &#183 3 weeks ago',
+    time: '2:57'
+  }
+]
