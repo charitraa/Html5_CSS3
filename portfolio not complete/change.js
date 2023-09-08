ffad75c168442d6d4fd58cf73399b1a52daa9f95
Charitra Shrestha <stharabi9862187405@gmail.com>
2023-09-08 07:58:39 +0545

    new

---
+++ portfolio not complete/change.js
@@ -0,0 +1,7 @@
+let typed = new typed('.changed', {
+  String: ['Software Engineer', 'Full Stack Engineer', 'app developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+})
