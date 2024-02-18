7f2ebeab737b58673f752d2f0f525fad81d4a025
charitraa <stharabi9862187405@gmail.com>
2024-02-18 23:39:36 +0545

    mew

--- portfolio not complete/script/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed('.change', {
-  strings: ['Software Developer', 'Full Stack Developer', 'Ethical Hacker'],
-  typeSpeed: 100,
-  backSpeed: 100,
-  backDelay: 1000,
-  loop: true
-})
