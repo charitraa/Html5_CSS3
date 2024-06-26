9b8a4271b9fdf516257236349add24e3c08d3026
charitraa <stharabi9862187405@gmail.com>
2024-06-26 09:49:39 +0545

    new

---
+++ index.js
@@ -0,0 +1,8 @@
+const purchase = {
+  purchase_id: 1,
+  last_name: 'john',
+  first_name: 'John',
+  birthday: 'September',
+  country: 'United states of America',
+  Date_of_purchase: '01-11-2015'
+}
